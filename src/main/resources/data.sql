c9cbc44b79e8074b8203ad92a754b9480f0ded11
Sridevi <sridevi.elangovan@gcitsolutions.com>
2022-09-30 12:32:30 +0530

    executed data.sql and schema.sql through code.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into client (id,system_oid,lmi_name,lmi_validation,status,created_on,created_by,modified_on,modified_by,name,email)
+values (2,'da','a','r',2,0 ,'1',0,'2','david','d@yahoo.com');
+
+insert into client_config(cid,key_parameters,value_parameters,lmi_id)
+values(2,'james','ja',1)
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,26 @@
+drop table if exists client_config;
 
-create table test(id integer(5) , name varchar(50), location varchar(50));
+drop table if exists client;
+
+create table client (
+                id integer(10) not null,
+                system_oid varchar(50) not null,
+                lmi_name varchar(50) not null,
+                lmi_validation tinytext not null,
+                status int(5) not null,
+                created_on datetime not null,
+                created_by varchar(45) not null,
+                modified_on datetime,
+                modified_by varchar(45) not null,
+                name varchar(255) not null,
+                email varchar(225) not null
+                );
+
+create table client_config(
+                cid integer not null,
+                key_parameters varchar(225),
+                value_parameters varchar(255),
+                primary key (cid),
+                foreign key (lmi_id) references client(id)
+);
+
